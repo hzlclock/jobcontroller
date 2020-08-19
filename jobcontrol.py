0ddc23a6109e44f02a7835d99f6ba97b1811be70
lindohe <hzlclock@gmail.com>
2020-08-19 21:12:33 +0800

    db log supportY

--- jobcontrol.py
+++ jobcontrol.py
@@ -5,10 +5,55 @@ import signal
 import time
 from time import gmtime, strftime
 import queue, sqlite3
-jobs=[]
-jobscnt=0
+import readline
+from io import StringIO
+
+vimrc='''" :Less
+" turn vim into a pager for psql aligned results 
+fun! Less()
+1
+set nocompatible
+set nowrap
+set scrollopt=ver
+set scrollbind
+
+nmap ^[OC zL
+nmap ^[OB ^E
+nmap ^[OD zH
+nmap ^[OA ^Y
+nmap <Left> 20zh
+nmap <Right> 20zl
+nmap <Up> 20k
+nmap <Down> 20j
+nmap <Space> <PageDown>
+" faster quit (I tend to forget about the upper panel)
+nmap q :qa!<CR>
+nmap Q :qa!<CR>
+endfun
+command! -nargs=0 Less call Less()
+'''
+with open('/tmp/.vimrc','w') as f:
+    f.write(vimrc)
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+con=sqlite3.connect("log.db")
+con.execute("create table if not exists jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, status text, cmd text, create_t text, run_t text, finish_t text)")
+cur=con.execute('select max(id) from jobs')
+jobs=cur.fetchall()
+con.close()
+if jobs[0][0] is None:
+    jobscnt=0
+else:
+    jobscnt=jobs[0][0]
+print("History jobs:", jobscnt)
+
+jobs={}
 jobq=queue.Queue()
-threadpoolmax=1
+threadpoolmax=2
 
 class trabajo:
     def __init__(self, cmd, pwd):
@@ -36,12 +81,18 @@ class trabajo:
             self.proc.wait()
             self.finishtime=strftime("%Y-%m-%d %H:%M:%S", gmtime())
             self.status+=1
+            with sqlite3.connect('log.db') as con:
+                con.execute('insert into jobs(status, cmd, create_t, run_t, finish_t) values(?,?,?,?,?)', 
+                    self.summary())
     def showstdout(self):
-        subprocess.run('vim -', shell=True, input=self.stdout)
+        subprocess.run('vim -c "Less" -u /tmp/.vimrc -', shell=True, input=self.stdout)
     # def showstderr(self):
     #     subprocess.run('vim -', shell=True, input=self.stderr)
     def summary(self):
-        return (self.statustext[self.status], self.cmd[:50], self.createtime, self.runtime, self.finishtime)
+        return (self.statustext[self.status], self.cmd, self.createtime, self.runtime, self.finishtime)
+    def shortsummary(self, maxlen=40):
+        shortcmd="\n".join(chunks(self.cmd, maxlen))
+        return (self.statustext[self.status], shortcmd, self.createtime, self.runtime, self.finishtime)
     def kill(self):
         if self.proc is not None:
             self.proc.kill()
@@ -60,21 +111,26 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def poolrunner():
-    print("start poolrunner")
+    global threadpoolmax
+    # print("start poolrunner")
     running_jobs=[]
     while True:
-        if jobq.empty():
-            time.sleep(1)
-        while len(running_jobs)<threadpoolmax:
-            q=jobq.get()
-            qthread=threading.Thread(target=q.run())
-            qthread.start()
-            running_jobs.append(qthread)
-            # print("run", q.summary())
-        for t in running_jobs:
-            if not t.is_alive():
-                running_jobs.remove(t)
-        time.sleep(1)
+        if not jobq.empty():
+            # print(len(running_jobs), threadpoolmax)
+            while len(running_jobs)<threadpoolmax:
+                q=jobq.get()
+                # print("1")
+                qthread=threading.Thread(target=q.run)
+                # print("2")
+                qthread.start()
+                # print("3")
+                running_jobs.append(qthread)
+                # print("4")
+                # print("run", q.summary())
+            for t in running_jobs:
+                if not t.is_alive():
+                    running_jobs.remove(t)
+        time.sleep(0.2)
 
 
 x = threading.Thread(target=poolrunner)
@@ -87,10 +143,12 @@ while True:
         try:
             os.chdir(os.path.join(os.getcwd(), cmd.split(' ')[1]))
         except: print("Cannot cd.")
-    elif cmd[0]=='!':
+    elif cmd=='ls':
+        print(subprocess.getoutput("ls"))
+    elif len(cmd)>0 and cmd[0]=='!':
         cmds=cmd.split(" ")
         if cmd=='!ls':
-            print(tabulate([(idx,*i.summary()) for idx,i in enumerate(jobs)], tablefmt="pretty",
+            print(tabulate([(k,*jobs[k].shortsummary()) for k in jobs], tablefmt="pretty",
                 headers=["ID","Status","Command","create","run","finished"]))
         elif cmds[0]=='!k':
             if len(cmds)==2 and cmds[1].isdigit():
@@ -98,6 +156,11 @@ while True:
         elif cmds[0]=='!s':
             if len(cmds)==2 and cmds[1].isdigit():
                 jobs[int(cmds[1])].showstdout()
+        elif cmds[0]=='!j':
+            if len(cmds)==2 and cmds[1].isdigit():
+                threadpoolmax=int(cmds[1])
+            else:
+                print("Thread pool size:", threadpoolmax)
         # elif cmds[0]=='!showstderr':
         #     if len(cmds)==2 and cmds[1].isdigit():
         #         jobs[int(cmds[1])].showstderr()
@@ -106,8 +169,13 @@ while True:
             print("\t!ls")
             print("\t!k <ID> kill")
             print("\t!s <ID> show stdout")
+            print("\t!j <NUM> modify max thread pool size")
+            print("\t!j         show max thread pool size")
             # print("\t!showstderr <ID>")
-    else:
+    elif len(cmd)>0:
         newjob=trabajo(cmd, os.getcwd())
-        jobs.append(newjob)
+        jobscnt+=1
+        jobs[jobscnt]=newjob
         jobq.put(newjob)
+    else:
+        print()
